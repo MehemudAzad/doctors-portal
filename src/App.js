adb12d306d3aef788b933f708d54e3f5aaa5c192
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-11-11 22:08:23 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className=''>
-     <h1>this is a new project </h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
